89ee72829ec415c5edba9d9881e4b4b460161474
Matheus Melo <teteuqe@hotmail.com>
2024-07-30 19:44:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Shreya Shree");
+
+const qrText = document.getElementById('qr-text')
+const sizes = document.getElementById('sizes')
+const generateBtn = document.getElementById('generateBtn')
+const downloadBtn = document.getElementById('downloadBtn')
+
+const qrContainer = document.querySelector('.qr-body');
+let size = sizes.value;
+generateBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (qrText.value.length > 0) {
+    generateQRCode();
+  }
+  else {
+    alert("Enter the text to generate qr code")
+  }
+
+});
+
+sizes.addEventListener('change', (e) => {
+  size = e.target.value;
+  isEmptyInput();
+});
+
+downloadBtn.addEventListener('click', () => {
+  let img = document.querySelector('.qr-body img');
+  if (img !== null) {
+    let imgArr = img.getAttribute('src');
+    downloadBtn.setAttribute('href', imgArr);
+  }
+  else {
+    downloadBtn.setAttribute('href', `${document.querySelector('canvas').toDataURL()}`);
+  }
+})
+
+function isEmptyInput() {
+  qrText.value.length > 0 ? generateQRCode() : alert("Enter the text to generate qr code");
+}
+
+function generateQRCode() {
+  qrContainer.innerHTML = "";
+  new QRCode(qrContainer, {
+    text: qrText.value,
+    height: size,
+    width: size,
+    colorLight: "#fff",
+    colorDark: "#000",
+  });
+}
